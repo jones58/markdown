4ced29bfadcef45bb5c27f626d2c5e0de95aec92
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-09 15:03:42 +0000

    default markdown

---
+++ src/default-markdown.tsx
@@ -0,0 +1,31 @@
+export const defaultMarkdown = `# Welcome to my Markdown Previewer!
+
+## This is a sub-heading
+
+### Links
+[Visit GitHub](https://github.com)
+
+### Inline Code
+Here's some inline code: \`const greeting = "Hello World!"\`
+
+### Code Block
+\`\`\`javascript
+function multiply(a, b) {
+  return a * b;
+}
+\`\`\`
+
+### List
+- First item
+- Second item
+- Third item
+
+### Blockquote
+> "Less, but better" - Dieter Rams
+
+### Image
+![React Logo](https://reactjs.org/logo-og.png)
+
+### Bold Text
+**This text is bold**
+`;
