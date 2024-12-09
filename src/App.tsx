09b3dc494ec6fbb963e0b83e06f201d2cb02c1c8
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-09 15:03:36 +0000

    add editor and preview logic, import default markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import { marked } from "marked";
+import { defaultMarkdown } from "./default-markdown";
+marked.setOptions({
+  breaks: true,
+});
+
 function App() {
-  return <>Hello!</>;
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <div className="editor-container">
+        <h2>Editor</h2>
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="preview-container">
+        <h2>Preview</h2>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{
+            __html: marked(markdown),
+          }}
+        />
+      </div>
+    </div>
+  );
 }
+
 export default App;
